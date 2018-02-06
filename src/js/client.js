ee0c2bf74d11eae9acf4c8af4186a26a18dad4a4
Matej Dujič <matej.dujic@visionect.com>
2018-02-06 18:09:35 +0100

    auth.jsx

--- src/js/client.js
+++ src/js/client.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import Layout from "./components/Layout";
+import Auth from "./routes/user/Auth.jsx";
+// import Layout from "./components/Layout";
 
 const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
+ReactDOM.render(<Auth/>, app);
---
+++ src/js/routes/user/Auth.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class Auth extends React.Component {
+    render () {
+        return <span>Hello</span>
+    }
+}
